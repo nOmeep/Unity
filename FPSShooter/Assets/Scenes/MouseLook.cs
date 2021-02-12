002395c755e3ad006cda83d40eae6c1c097bfdcc
Sevastyan <sevapanov@yandex.ru>
2021-02-12 18:03:04 +0300

    работа с 3D чуть-чуть

---
+++ FPSShooter/Assets/Scenes/MouseLook.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    public enum RotationAxes
+    {
+        MouseXandY = 0,
+        MouseX = 1,
+        MouseY = 2
+    };
+
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            // only for horizontal
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            // only for vertical
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            // combine 
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
---
+++ FPSShooter/Assets/Scenes/Spin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 0.3f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0, Space.World);
+    }
+}
